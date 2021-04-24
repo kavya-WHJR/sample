de397b378b4e5a866853c0c54b2c6ca68d2a0975
kavya-WHJR <72651349+kavya-WHJR@users.noreply.github.com>
2021-04-24 14:12:10 +0530

    Update sketch.js
    chng bg to blue

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup(){
 }
 
 function draw(){
-    background("orange");
+    background("blue");
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
